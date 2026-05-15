a54d7fef47203b5af9ff7ff736331cd754f3d0ef
Danny Burgos <Presley@mac.home>
2026-05-15 12:25:38 -0400

    Update 2026-05-15 12:25

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,4 @@ git add -A
 git commit -m "Update $(date '+%Y-%m-%d %H:%M')"
 git push -u origin main
 
-firebase deploy --only hosting
-
-echo "Done!"
+echo "Done! Live at https://bannydurgos.github.io/DBT_DIARY/"
